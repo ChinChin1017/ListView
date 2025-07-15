42036b1d3c803b128381743452458b3cefa5a69b
ChinChin1017 <jeff911017@gmail.com>
2025-07-15 19:14:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/steak/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.steak;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Optional;
+
+public class MainActivity extends AppCompatActivity {
+    Spinner spinner;
+    Button button;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        spinner = findViewById(R.id.spinner);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                String selectodoption = spinner.getSelectedItem().toString();
+//                textView.setText("牛排" + selectodoption);
+                int ops = spinner.getSelectedItemPosition();
+                String options[]= getResources().getStringArray(R.array.Spinner);
+                textView.setText("牛排要" + options[ops]);
+            }
+        });
+
+
+    }
+}
